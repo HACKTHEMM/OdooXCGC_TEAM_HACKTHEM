1a4d23f41428449ac4eb4185ae1f8091b79b5ab0
Raghavendrabaheti <bahetiraghav990@gmail.com>
2025-08-02 17:11:43 +0530

    fully fixed

--- frontend/src/components/__tests__/connection-test.test.tsx
+++ frontend/src/components/__tests__/connection-test.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen, waitFor } from '@testing-library/react'
+import '@testing-library/jest-dom'
 import userEvent from '@testing-library/user-event'
 import ConnectionTest from '../connection-test'
 
